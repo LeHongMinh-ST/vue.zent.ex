de87aa58c3411aaa769f79ed61409fc10446cf7a
Hồng Minh Lê <5634046-minhlh@users.noreply.gitlab.com>
2021-01-22 23:23:29 +0700

    sua content

--- src/components/Buoi5/admin/Statistic.vue
+++ src/components/Buoi5/admin/Statistic.vue
@@ -5,33 +5,25 @@
     </template>
     <template v-slot:main-content>
       <el-main>
-        <el-table
-            :data="tableData"
-            border
-            style="width: 100%">
-          <el-table-column
-              prop="date"
-              label="Date"
-              width="180">
-          </el-table-column>
-          <el-table-column
-              prop="name"
-              label="Name"
-              width="180">
-          </el-table-column>
-          <el-table-column
-              @size-change="handleSizeChange"
-              prop="address"
-              label="Address">
-          </el-table-column>
-        </el-table>
-        <div class="paginate">
-          <el-pagination
-              background
-              layout="total,prev, pager, next,jumper"
-              :total="50">
-          </el-pagination>
-        </div>
+        <el-collapse v-model="activeNames" @change="handleChange">
+          <el-collapse-item title="Consistency" name="1">
+            <div>Consistent with real life: in line with the process and logic of real life, and comply with languages and habits that the users are used to;</div>
+            <div>Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc.</div>
+          </el-collapse-item>
+          <el-collapse-item title="Feedback" name="2">
+            <div>Operation feedback: enable the users to clearly perceive their operations by style updates and interactive effects;</div>
+            <div>Visual feedback: reflect current state by updating or rearranging elements of the page.</div>
+          </el-collapse-item>
+          <el-collapse-item title="Efficiency" name="3">
+            <div>Simplify the process: keep operating process simple and intuitive;</div>
+            <div>Definite and clear: enunciate your intentions clearly so that the users can quickly understand and make decisions;</div>
+            <div>Easy to identify: the interface should be straightforward, which helps the users to identify and frees them from memorizing and recalling.</div>
+          </el-collapse-item>
+          <el-collapse-item title="Controllability" name="4">
+            <div>Decision making: giving advices about operations is acceptable, but do not make decisions for the users;</div>
+            <div>Controlled consequences: users should be granted the freedom to operate, including canceling, aborting or terminating current operation.</div>
+          </el-collapse-item>
+        </el-collapse>
 
       </el-main>
     </template>
